b84c910ce28e86df2dda34e8be807a0c68f0e1a3
varas-c <Christian.varas.93@gmail.com>
2017-10-08 23:00:30 -0300

    Cuentas

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -15,44 +15,42 @@ import model.InvalidFileException;
 import model.Company;
 
 public class Server {
-	public static void main(String[] args) {
+	public static List<Company> companies;
+	
+	public static void main(String[] args) throws InvalidFileException, IOException {
+		CompanyLoader a = new CompanyLoader();
+		companies = a.getCompanyList("src/test/resources/cuentasHector3.json");
 		Spark.port(9000);
 		DebugScreen.enableDebugScreen();
 		HandlebarsTemplateEngine engine = 
 				new HandlebarsTemplateEngine();
 		Spark.get("/hello", (request, response) -> Server.show(request, response), engine);
 		Spark.get("/login", (request, response) -> Server.login(request, response), engine);
-		Spark.get("/tabla", (request, response) -> Server.tabla(request, response), engine);
+		Spark.get("/cuentas", (request, response) -> Server.cuentas(request, response), engine);
 		Spark.stop();
 	}
 	
-	private static ModelAndView tabla(Request request, Response response) throws InvalidFileException, IOException {
-		HashMap<String,Object> viewModel = new HashMap<>();
-		CompanyLoader a = new CompanyLoader();
-		List<Company> companies = a.getCompanyList("/home/chris/Documents/DisenoSistemas/2017-jm-group-02/src/test/resources/cuentasHector3.json");
+	private static ModelAndView cuentas(Request request, Response response) throws InvalidFileException, IOException {
+		String companyName = request.queryParams("company");
+		Company company = companies.stream().filter(x -> x.getName().equals(companyName)).findFirst().orElse(null);
 		
-		viewModel.put("companies", companies);
+		HashMap<String, Object> viewModel = new HashMap<>();
+		viewModel.put("accounts", company.getAccounts());
 		
 		return new ModelAndView(viewModel, "cuentas.hbs");
 	}
 
 	private static ModelAndView login(Request request, Response response) throws InvalidFileException, IOException {
 		
-		System.out.println("LOGEANDO");
 		HashMap<String, Object> viewModel = new HashMap<>();
 		String user = request.queryParams("user");
 		String pass = request.queryParams("pass");
-		System.out.println(user);
 		
 		if(user.equals("Chris")) {
 			System.out.println("OK!");
 			return new ModelAndView(viewModel, "badLogin.hbs"); 
 		}
 		
-		CompanyLoader a = new CompanyLoader();
-		List<Company> companies = a.getCompanyList("src/test/resources/cuentasHector3.json");
-		
-		System.out.println(companies.get(0).getName());
 		viewModel.put("companies", companies);
 		
 		return new ModelAndView(viewModel, "index.hbs");
